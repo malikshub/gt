9b6d1091305e4b568970b03b158bb619f03efe06
Shubham Malik <malikshub@gmail.com>
2021-04-20 23:16:02 +0530

    Blog-pages added Nav and foot fixed

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,11 @@
-$('.datepicker').each(function(){
-	var picker = new Pikaday({
-		field: this
-	});
-});
+// Custom theme code
+
+if (document.getElementsByClassName('clean-gallery').length > 0) {
+   baguetteBox.run('.clean-gallery', { animation: 'slideIn' });
+}
+
+if (document.getElementsByClassName('clean-product').length > 0) {
+    window.onload = function() {
+        vanillaZoom.init('#product-preview');
+    };
+}
